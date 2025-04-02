65f61a9b0b17ba2fd0d514f90eabb332485e46b8
Harrison Tran <atxharrison.tran@gmail.com>
2025-04-02 17:38:51 -0500

    Change positioning logic
    Player position is now sprite center rather than top left of sprite

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from tronKart import TronKart
 
+
 def main():
     game = TronKart()
     game.run()
 
+
 if __name__ == '__main__':
-    main()  
+    main()
--- player.py
+++ player.py
@@ -2,11 +2,12 @@ import math
 import pygame
 from trail import Trail
 
-MIN_SPEED = 6
-MAX_SPEED = 16
-BLUE = (0, 0, 255)
-TRAIL_WIDTH = 5
-IMAGE_SCALE = (100, 100)
+MIN_SPEED: int = 6
+MAX_SPEED: int = 16
+BLUE: (int, int, int) = (0, 0, 255)
+TRAIL_WIDTH: int = 5
+IMAGE_SCALE: int = (100, 100)
+
 
 class Player:
     def __init__(self, x, y, direction_angle, speed, image):
@@ -21,15 +22,16 @@ class Player:
     def change_direction(self, angle_change):  # angle is in degrees
         self.direction_angle = (self.direction_angle + angle_change) % 360
 
-
     def change_speed(self, speed_change):
         self.speed = max(min(self.speed + speed_change, MAX_SPEED), MIN_SPEED)
 
     def update(self, width, height):
         self.x = (self.x + self.speed * math.cos(math.radians(self.direction_angle))) % width
         self.y = (self.y - self.speed * math.sin(math.radians(self.direction_angle))) % height # minus because y is inverted (pygame dumb)
-        self.trail.add_point((self.x + self.image.get_width() / 2, self.y + self.image.get_height() / 2))
+        self.trail.add_point((self.x, self.y))
 
     def draw(self, window):
         self.trail.draw(window)
-        window.blit(self.image, (self.x, self.y))
+        window.blit(self.image,
+                    (self.x - self.image.get_width() / 2,
+                     self.y - self.image.get_height() / 2))
